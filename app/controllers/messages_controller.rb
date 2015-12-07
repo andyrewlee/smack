82d7d96af67a91e8b4c7157ff0346b40ac07e4b0
Andrew Lee <andyrewlee@gmail.com>
2015-12-06 17:57:12 -0800

    Add new channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,18 @@ class MessagesController < ApplicationController
     @users = User.all
     @message = Message.find(params[:id])
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+    redirect_to @message
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:name)
+    end
 end
