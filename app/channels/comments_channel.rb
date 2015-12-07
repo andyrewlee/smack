ceb218efa63b4ea75c70d7975999b6733f39da80
Andrew Lee <andyrewlee@gmail.com>
2015-12-06 22:05:05 -0800

    Log all streams user is subscribed to for debugging

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -2,6 +2,10 @@ class CommentsChannel < ApplicationCable::Channel
   def follow(data)
     stop_all_streams
     stream_from "messages:#{data['message_id'].to_i}:comments"
+
+    streams.each do |stream|
+      puts stream
+    end
   end
 
   def unfollow
