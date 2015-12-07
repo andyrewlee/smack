dcb60529ade6582d0fb98106114afb4f88a81541
Andrew Lee <andyrewlee@gmail.com>
2015-12-06 17:41:17 -0800

    Get log out working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create]
-  resources :messages, only: [:show] do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :messages, only: [:show, :new, :create] do
     resources :comments, only: [:create]
   end
   resources :users, only: [:new, :create, :show]
