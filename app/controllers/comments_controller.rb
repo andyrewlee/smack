c086fa705d2d120b399ab55482b0b0d5933f0c8e
Andrew Lee <andyrewlee@gmail.com>
2015-12-06 17:17:06 -0800

    Persist chat

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   def create
+    message = Message.find(params[:message_id])
+    message.comments.create(comment: params[:comment][:comment], user: current_user)
     ActionCable.server.broadcast "messages:#{params[:message_id].to_i}:comments",
       comment: params[:comment][:comment],
       username: current_user.username
